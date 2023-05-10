71600e47d21418d01b052129277e9c2bbbe5d67e
Martin <samkergachu@gmail.com>
2023-05-10 14:40:25 +0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,9 +4,9 @@
 
 # Do not use this in production. Sample use only. 
 
-# Author Martin Gachu (IBM)
+# Author Upkar Lidder (IBM)
 # Additional Authors
-# Upkar Lidder (IBM)
+# samkergachu
 
 # Input
 # p, principal amount
